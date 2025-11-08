57dbdcaca44812708aef325934c0bd89b1b5e8aa
nam2107 <nam.vuong1508@gmail.com>
2025-11-08 16:55:41 +0700

    Initial commit

---
+++ ex1/lane_detection-master/lane_detection-master/lane_detection.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindow()
+                                    
